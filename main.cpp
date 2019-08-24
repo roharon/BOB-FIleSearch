9cb4655186a7ecaf1f5110368c9d969685aa994b
roharon <roharon@studentpartner.com>
2019-08-24 14:34:24 +0900

    create: Repository

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include "cppcore/Src/000_String/000_String.h"
+#include "cppcore/Inc/cppcore.h"
+using namespace std;
+using namespace core;
+
+int main(){
+    FindFirstFile("a", (ST_FILE_FINDDATAA *) "/a");
+}
